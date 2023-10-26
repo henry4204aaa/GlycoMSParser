c8a9b2e9ebba50000dd89780deb39f30829cf733
henry4204aaa <henry4204aaa@hotmail.com>
2023-10-26 21:12:58 +0900

    snapshot20231026

--- src/comparepeaklist.py
+++ src/comparepeaklist.py
@@ -14,10 +14,10 @@ try:
 except ImportError:
     raise ImportError('Please install tkinter')
 
-inputlist = []
-comparelist = []
-inputintensitylist = []
-ninputintensitylist = []
+#inputlist = []
+#comparelist = []
+#inputintensitylist = []
+#ninputintensitylist = []
 #force int type after validate it's not NoneType
 def ppmcalculation(userdefinedppm=None):
     defineppmflag = True
@@ -87,25 +87,32 @@ def comparepeaklist(inputlist, comparelist):
 #can run faster (not sure if the numbers will be compared
 #as string or float number
 
-def comparepeaklistppm(inputlist, comparelist, ppm, inputintensitylist):
+def comparepeaklistppm(inputlist = [], comparelistglob = [], ppm = 500, inputintensitylist = [], debug = False, autoall= False):
+    comparelist=comparelistglob[:] #prevent same reference in for loop #fix in 20231012
     #pre-assign input list and compare list
+    if debug:
+         print(f"inputlist in another file is {inputlist}")
+         print(f"comparelist in another file is {comparelist}")
     if (inputlist == []):
+        #print(f"inputlist in condition {inputlist}")
         inputlist = [344, 344.16, 344.19, 344.25, 345, 376, 432, 464]
-        print('use default inputlist', inputlist)
+        #print('use default inputlist', inputlist)
     else:
         #print('inputlist is', inputlist)
         pass
     if (comparelist == []):
+        #print(f"comparelist in condition {comparelist}")
         comparelist = [344.17, 374.18, 376.20, 406.21, 432.22, 450.23, 464.25, 580.30, 793.38, 825.42]
-        print('use default comparelist', comparelist)
+        #print('use default comparelist', comparelist)
     else:
         #print('comparelist is', comparelist)
         pass
     if (inputintensitylist == []):
+        #print(f"inputintensitylist in condition {inputintensitylist}")
         inputintensitylist = [500, 1000, 34567, 43210, 666666, 1, 810, 114514, 100000, 100000000000]
-        #print('use default comparelist', comparelist)
+        #print('use default comparelist', inputintensitylist)
     else:
-        #print('comparelist is', comparelist)
+        #print('inputintensitylist in another file is', inputintensitylist)
         pass
     #ppm = ppmcalculation()
     #normalize from inputintensitylist
@@ -118,6 +125,8 @@ def comparepeaklistppm(inputlist, comparelist, ppm, inputintensitylist):
     for i in range(len(inputintensitylist)):
         ninputintensitylist.append((inputintensitylist[i]/maxin)*100)
     #for test the normalized bug (fixed)
+    if autoall:
+        print("maxn of inputlist while not using comparelist")
     '''
     if str(maxin)== str(3798419.75):
         print('hey', ninputintensitylist)
